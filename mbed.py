a4c9a1d852bd146563e69ad66b3ccc4992a718cf
Ivan Kravets <me@ikravets.com>
2018-07-27 00:38:50 +0300

    Update mbed.py

--- mbed.py
+++ mbed.py
@@ -88,12 +88,40 @@ def get_dynamic_manifest(name, config, extra_inc_dirs=[]):
     for f in src_files:
         manifest['build']['srcFilter'].extend([" +<%s>" % f])
 
-    if name == "LWIP":
-        manifest['dependencies'] = {"mbed-events": "*"}
+    manifest['build']['libLDFMode'] = "deep+"
 
-    manifest['build']['libLDFMode'] = "chain+"
+    # Implicit dependencies:
+    # OnboardNetworkStack::get_default_instance
+    # mbed::mbed_event_queue
+    if name == "netsocket":
+        manifest['build']['flags'].extend(["-DMBED_CONF_EVENTS_PRESENT"])
+        manifest['dependencies'] = {
+            "mbed-LWIP": "*",
+            "mbed-events": "*"
+        }
+
+    # arm_random_module_init
+    if name == "mbed-client-randlib":
+        manifest['dependencies'] = {"mbed-nanostack": "*"}
 
     return manifest
+
+
+def process_global_lib(libname, mbed_config):
+    if not libname or not mbed_config:
+        return
+    lib_config = mbed_config.get("libs").get(libname, {})
+    if not lib_config:
+        return
+    lib_includes = [
+        join(FRAMEWORK_DIR, lib_config.get("dir"), f)
+        for f in lib_config.get("inc_dirs")
+    ]
+
+    env.Append(
+        CPPPATH=lib_includes,
+        LIB_DEPS=["mbed-%s" % libname]
+    )
 
 
 variants_remap = util.load_json(
@@ -137,12 +165,6 @@ env.ProcessUnFlags(env.get("BUILD_UNFLAGS"))
 # apply user flags
 env.ProcessFlags(env.get("BUILD_FLAGS"))
 
-MBED_RTOS = "PIO_FRAMEWORK_MBED_RTOS_PRESENT" in env.Flatten(
-    env.get("CPPDEFINES", []))
-
-if MBED_RTOS:
-    env.Append(CPPDEFINES=["MBED_CONF_RTOS_PRESENT"])
-
 #
 # Process libraries
 #
@@ -150,6 +172,7 @@ if MBED_RTOS:
 # There is no difference in processing between lib and feature
 libs = mbed_config.get("libs").copy()
 libs.update(mbed_config.get("features"))
+libs.update(mbed_config.get("frameworks"))
 
 #
 # Process Core files from framework
@@ -170,10 +193,19 @@ env.Append(CPPPATH=[
     join(FRAMEWORK_DIR, "platformio", "variants", variant)
 ])
 
-# If RTOS is enabled then some of the files from Core depdend on it
+MBED_RTOS = "PIO_FRAMEWORK_MBED_RTOS_PRESENT" in env.Flatten(
+    env.get("CPPDEFINES", []))
+
 if MBED_RTOS:
-    for d in mbed_config.get("libs").get("rtos").get("inc_dirs"):
-        env.Append(CPPPATH=[join(FRAMEWORK_DIR, "rtos", d)])
+    env.Append(CPPDEFINES=["MBED_CONF_RTOS_PRESENT"])
+    process_global_lib("rtos", mbed_config)
+
+MBED_EVENTS = "PIO_FRAMEWORK_MBED_EVENTS_PRESENT" in env.Flatten(
+    env.get("CPPDEFINES", []))
+
+if MBED_EVENTS:
+    env.Append(CPPDEFINES=["MBED_CONF_EVENTS_PRESENT"])
+    process_global_lib("events", mbed_config)
 
 core_src_files = mbed_config.get("core").get("s_sources") + mbed_config.get(
     "core").get("c_sources") + mbed_config.get("core").get("cpp_sources")
@@ -196,10 +228,7 @@ if "nordicnrf5" in env.get("PIOPLATFORM"):
 # Generate linker script
 #
 
-if isfile(join(env.PioPlatform().get_dir(), "ldscripts", env.BoardConfig().get("build.ldscript", ""))):
-    env.Replace(LDSCRIPT_PATH=join(env.PioPlatform().get_dir(), "ldscripts", env.BoardConfig().get("build.ldscript")))
-else:
-    env.Replace(LDSCRIPT_PATH=join(FRAMEWORK_DIR, mbed_config.get("ldscript")))
+env.Replace(LDSCRIPT_PATH=join(FRAMEWORK_DIR, mbed_config.get("ldscript")))
 if not env.get("LDSCRIPT_PATH"):
     sys.stderr.write("Cannot find linker script for your board!\n")
     env.Exit(1)
@@ -222,25 +251,10 @@ env.Replace(LDSCRIPT_PATH=linker_script)
 
 for lib, lib_config in libs.items():
     extra_includes = []
-    if lib == "events" and not MBED_RTOS:
-        # Manually handle dependency on rtos lib
-        extra_includes = [
-            join(FRAMEWORK_DIR,
-                 mbed_config.get("libs").get("rtos").get("dir"), f)
-            for f in mbed_config.get("libs").get("rtos").get("inc_dirs")
-        ]
-
     env.Append(EXTRA_LIB_BUILDERS=[
         CustomLibBuilder(env, join(FRAMEWORK_DIR, lib_config.get("dir")),
                          get_dynamic_manifest(lib, lib_config, extra_includes))
     ])
 
-# Add RTOS library only when a user requested it
-if MBED_RTOS:
-    rtos_config = mbed_config.get("libs").get("rtos")
-    env.Append(EXTRA_LIB_BUILDERS=[
-        CustomLibBuilder(env, join(FRAMEWORK_DIR, rtos_config.get("dir")),
-                         get_dynamic_manifest("rtos", rtos_config))
-    ])
-
-    env.Append(LIB_DEPS=["mbed-rtos"])
+    if lib == "mbedtls":
+        env.Append(LIB_DEPS=["mbed-mbedtls"])
