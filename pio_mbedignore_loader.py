8ca976f30b6857844b0bf33287503d3370e9b324
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-10-03 12:07:33 +0200

    Implement .mbedignore loader
    This class extends MbedIgnoreSet from MBED OS Python API. I extended it
    because it already has `.mbedignore` loader functions, but the class
    doesn't expose any method to get the collected patterns.
    
    The class has methods that are functions with side effects. This is not
    clean, but there is no clean way to utilize the MbedIgnoreSet methods
    without side effects. The used MbedIgnoreSet.add_mbedignore() function
    mutates self._ignore_patterns, so performing any other operations on
    that member will, of course, have side effects.
    
    The collect_ignore_patterns() implement caching. When the ignore
    patterns has been collected once, they won't be recollected. The result
    from the first collection is stored internally and returned on each
    consecutive call to the method.
    
    The result of collect_ignore_patterns() is supposed to be provided to
    Resources.add_ignore_patterns() function. To be more specific, this
    method will be called on the extender of Resources, which is
    MbedResourcesFixedPath. The function expects patterns in specific
    format, I explain it along with
    _remove_first_directory_name_from_patterns() below.
    
    The methods following methods mutate self._ignore_patterns:
    
    * _remove_mbed_framework_prefix_from_patterns() - performs filtering
    which removes 'mbed-os/' or 'framework-mbed/' prefix from the ignore
    patterns.
    * _remove_first_directory_name_from_patterns() - removes first directory
    name from the patterns. This comes from experimenting. I have checked
    what patterns does Resources.add_ignore_patterns() expect. The patterns
    are only obeyed (directories are ignored) when there is no first folder
    (directory) name in the pattern. So, e.g. 'features/cellular/*' will not
    be properly applied, but 'cellular/*' will. Finally, the method from
    this bullet, will turn the pattern 'features/cellular/*' to
    'cellular/*'.

---
+++ pio_mbedignore_loader.py
@@ -0,0 +1,74 @@
+# Copyright 2019-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from tools.resources import MbedIgnoreSet
+
+
+class PioMbedignoreLoader(MbedIgnoreSet):
+    """Loads ignore patterns from .mbedignore file
+
+    It takes the path to the `.mbedignore` file in the constructor.
+    The collect_ignore_patterns() will return the list of the patterns
+    from the file. For each pattern the 'mbed-os/' or 'framework-mbed/' prefix
+    will be removed if specified. Since originial MbedIgnoreSet computes
+    relative paths starting from the first parent directory,
+    the parent directory name will be removed, so e.g. paterrns:
+
+        mbed-os/features/cellular/*
+        framework-mbed/features/cellular/*
+        features/cellular/*
+
+    will be turned to:
+
+        cellular/*
+
+    """
+
+    def __init__(self, mbedignore_path):
+        self._mbedignore_path = mbedignore_path
+        self._is_patterns_collected = False
+        super().__init__()
+
+    def collect_ignore_patterns(self):
+        if self._is_patterns_collected:
+            return self._ignore_patterns
+
+        self._collect_patterns_from_mbedignore_file_and_save_them()
+        self._remove_mbed_framework_prefix_from_patterns()
+        self._remove_first_directory_name_from_patterns()
+        self._is_patterns_collected = True
+        return self._ignore_patterns
+
+    def _collect_patterns_from_mbedignore_file_and_save_them(self):
+        try:
+            self.add_mbedignore('.', self._mbedignore_path)
+            print('Loaded .mbedignore patterns from: {}'.format(
+                self._mbedignore_path))
+        except FileNotFoundError:
+            print('No .mbedignore found under {}'.format(
+                self._mbedignore_path))
+
+    def _remove_mbed_framework_prefix_from_patterns(self):
+        paths_without_mbed_framework_prefix_iterator = map(
+            lambda p: p.replace('mbed-os/', '').replace('framework-mbed/', ''),
+            self._ignore_patterns)
+        self._ignore_patterns = list(
+            paths_without_mbed_framework_prefix_iterator)
+
+    def _remove_first_directory_name_from_patterns(self):
+        paths_without_first_directory_name_iterator = map(
+            lambda p: p[p.find('/')+1:],
+            self._ignore_patterns)
+        self._ignore_patterns = list(
+            paths_without_first_directory_name_iterator)
